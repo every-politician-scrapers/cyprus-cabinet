46f2e31f6621437b364919985c1e761966f08cd6
Tony Bowden <tony@tmtm.com>
2022-07-28 22:34:43 +0300

    refresh data

--- sources/legislature-official/compare.sh
+++ sources/legislature-official/compare.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-bundle exec ruby scraper.rb | qsv select id,name,faktion,komisjon | qsv rename id,itemLabel,partyLabel,commission > scraped.csv
+bundle exec ruby scraper.rb | qsv select name | qsv rename itemLabel > scraped.csv
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s itemLabel,startDate > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s itemlabel | tee diff.csv
 
